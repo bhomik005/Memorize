e43d755a865265376c227ed34b74bd428ce45960
bhomik005 <bhomik005@gmail.com>
2024-09-23 15:53:12 +1000

    added bunch of stuff

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -19,18 +19,53 @@ import SwiftUI
  Array out of bounds is a crasher in swift
  */
 struct ContentView: View {
-    let emojis : [String] = ["👻", "🎃", "💀", "🕷️", "🤖", "🚀"]
+    let emojis : [String] = ["👻", "🎃", "💀", "🕷️", "🤖", "🚀", "🚎", "👽", "😭"]
+    @State var cardCount: Int = 4
     var body: some View {
-        HStack {
-            // we want a for loop here
-            // index in - it is an argument to closure
-            // emojis.indices - returns the range of array
-            ForEach(emojis.indices, id: \.self ) { index in
+        VStack {
+            ScrollView {
+                cards
+            }
+            Spacer()
+            cardCountAdjusters
+        }
+        .padding()
+    }
+    
+    var cards: some View {
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]) {
+            ForEach(0..<cardCount, id: \.self ) { index in
                 CardView(content: emojis[index])
+                    .aspectRatio(2/3, contentMode: .fit)
             }
         }
         .foregroundColor(Color.orange)
-        .padding()
+    }
+    
+    var cardCountAdjusters: some View {
+        HStack {
+            cardRemover
+            Spacer()
+            cardAdder
+        }
+        .imageScale(.large)
+        .font(.largeTitle)
+    }
+    func cardCountAdjuster(by offset: Int, symbol: String) -> some View {
+        Button(action: {
+            cardCount += offset
+
+        }, label: {
+            Image(systemName: symbol)
+        })
+        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
+    }
+    var cardRemover: some View {
+        cardCountAdjuster(by: -1, symbol: "rectangle.stack.badge.minus.fill")
+    }
+    
+    var cardAdder: some View {
+        cardCountAdjuster(by: 1, symbol: "rectangle.stack.badge.plus.fill")
     }
 }
 // let is a constant (cannot change once assigned a value)
@@ -42,13 +77,14 @@ struct CardView: View {
         ZStack {
             // type inference in Swift
             let base = RoundedRectangle(cornerRadius: 12)
-            if isFaceUp {
+            // Group is a bag of legos - allows us to apply opacity
+            Group {
                 base.fill(.white)
                 base.strokeBorder(lineWidth: 2)
                 Text(content).font(.largeTitle)
-            } else {
-                base.fill() // fill is a default
             }
+            .opacity(isFaceUp ? 1 : 0)
+            base.fill().opacity(isFaceUp ? 0 : 1) // 0 -> fully transparent, 1 -> fully opaque
         }
         .onTapGesture {
             isFaceUp.toggle()
